fcad40b4fdfc242af9e6f265d8df5739c883488b
Misha Dzhemula <medvedmisha23@gmail.com>
2023-02-02 20:22:19 +0200

    db connected

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,8 @@
 exports.__esModule = true;
 exports.sequelize = void 0;
 var sequelize_1 = require("sequelize");
-exports.sequelize = new sequelize_1.Sequelize('postgres', 'postgres', 'Test1234', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
+// export const sequelize = new Sequelize('postgres', 'postgres', 'Test1234', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+// });
+exports.sequelize = new sequelize_1.Sequelize('postgres://DarkMistyRoom:fQw5qvGy0KIM@ep-calm-wave-927722.eu-central-1.aws.neon.tech/neondb?ssl=true');
