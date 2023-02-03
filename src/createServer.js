7d723f827e8c9381b256e135c3065b46b265767b
Misha Dzhemula <medvedmisha23@gmail.com>
2023-02-03 11:12:02 +0200

    6

--- src/createServer.js
+++ src/createServer.js
@@ -18,7 +18,20 @@ var Server = /** @class */ (function () {
         this.addRouter('/comments', express.json(), comments_1.commentsRouter.router);
     };
     Server.prototype.useCors = function () {
-        this.app.use(cors());
+        // this.app.use(cors());
+        this.app.use(function (req, res, next) {
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', 'true');
+            // Pass to next layer of middleware
+            next();
+        });
     };
     Server.prototype.addRouter = function (path, middleware, router) {
         this.app.use(path, middleware, router);
